7e6a4278c26b689d9d632a6d6f07f990f30faf49
SerG <serg.arnisha@gmail.com>
2023-01-02 11:46:28 +0100

    add footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,7 +1,26 @@
 import "./footer.css";
 
+import Nav from "react-bootstrap/Nav";
+
 const Footer = () => {
-  return <div className="footer"></div>;
+  return (
+    <div id="footer">
+      <Nav defaultActiveKey="/home" as="ul" id="foot-nav">
+        <Nav.Item as="li">
+          <Nav.Link href="/home" id="img-footer">
+            <div className="img-footer"></div>Coffee house
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+          <Nav.Link eventKey="link-1">Our coffee</Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+          <Nav.Link eventKey="link-2">For your pleasure</Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <div className="imgLogoDarck"></div>
+    </div>
+  );
 };
 
 export default Footer;
