fc6060fe1e83984b58f49c24d2614deeadfeab22
SerG <serg.arnisha@gmail.com>
2023-01-04 17:39:40 +0100

    img for Best goods module

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,5 @@
+import { Component } from "react";
+
 import Header from "../header/header";
 import AboutUs from "../aboutUs/aboutUs";
 import OurBest from "../ourBest/ourBest";
@@ -8,33 +10,39 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col } from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <Container fluid>
-        <Row>
-          <Col>
-            <Header />
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <AboutUs />
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <OurBest />
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Footer />
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="App">
+        <Container fluid>
+          <Row>
+            <Col>
+              <Header />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <AboutUs />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <OurBest />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Footer />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ourBest/ourBest.js
+++ src/components/ourBest/ourBest.js
@@ -2,29 +2,49 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 import "./ourBest.css";
+import aromistico from "./ourBestImg/aromistico.png";
+import presto from "./ourBestImg/presto.png";
+import solimo from "./ourBestImg/solimo.png";
 
 const OurBest = () => {
+  const img = [solimo, presto, aromistico];
+
+  const goodsImg = [
+    {
+      goodsName: "Solimo Coffee Beans 2 kg",
+      price: 10.73,
+      number: 1,
+      name: "solimo",
+    },
+    {
+      goodsName: "Presto Coffee Beans 1 kg",
+      price: 15.99,
+      number: 2,
+      name: "presto",
+    },
+    {
+      goodsName: "AROMISTICO Coffee 1kg",
+      price: 6.99,
+      number: 3,
+      name: "aromistico",
+    },
+  ];
+
+  const col = goodsImg.map(({ goodsName, price, number, name }) => {
+    return (
+      <Col className="col">
+        <img className="goods-img" src={img[number - 1]} alt={name} />
+        <p className="goods-name">{goodsName}</p>
+        <p className="goods-price">{`${price}$`}</p>
+      </Col>
+    );
+  });
+
   return (
     <div className="ourBest">
       <h4> Our best</h4>
       <div className="containerOurBest">
-        <Row id="best">
-          <Col className="col">
-            <div className="goods-img"></div>
-            <p className="goods-name">Solimo Coffee Beans 2 kg</p>
-            <p className="goods-price">10.73$</p>
-          </Col>
-          <Col className="col">
-            <div className="goods-img"></div>
-            <p className="goods-name">Presto Coffee Beans 1 kg</p>
-            <p className="goods-price">15.99$</p>
-          </Col>
-          <Col className="col">
-            <div className="goods-img"></div>
-            <p className="goods-name">AROMISTICO Coffee 1kg</p>
-            <p className="goods-price">6.99$</p>
-          </Col>
-        </Row>
+        <Row id="best">{col}</Row>
       </div>
     </div>
   );
